816823ae2d6c4aae210f42910b09e0e8252049d4
fredf <fredflorescfa@gmail.com>
2020-04-19 12:28:36 -0700

    added metadata

--- alpha_vantage_data.py
+++ alpha_vantage_data.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python
+
+__author__ = 'Fred Flores'
+__version__ = '0.0.1'
+__date__ = '2020-04-19'
+__email__ = 'fredflorescfa@gmail.com'
+
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 
 # Your key here
-key = 'C1MLXDST5B1WH97M'  # fredflorescfa@gmail.com 2020-04-19
+key = 'C1MLXDST5B1WH97M'  # created 2020-04-19 fredflorescfa@gmail.com
 
 # Chose your output format, or default to JSON (python dict)
 ts = TimeSeries(key, output_format='pandas')
