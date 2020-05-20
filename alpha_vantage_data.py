5dda1f8eb50a1c21be97a84e7c1cfa145745a170
fredf <fredflorescfa@gmail.com>
2020-05-19 21:30:07 -0700

    cleaned up alpha_vantage script

--- alpha_vantage_data.py
+++ alpha_vantage_data.py
@@ -16,16 +16,3 @@ key = 'C1MLXDST5B1WH97M'  # created 2020-04-19 fredflorescfa@gmail.com
 # Chose your output format, or default to JSON (python dict)
 ts = TimeSeries(key, output_format='pandas')
 ti = TechIndicators(key)
-
-# Get the data, returns a tuple
-# aapl_data is a pandas dataframe, aapl_meta_data is a dict
-aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL')
-# aapl_sma is a dict, aapl_meta_sma also a dict
-aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
-
-# Visualization
-figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
-aapl_data['4. close'].plot()
-plt.tight_layout()
-plt.grid()
-plt.show()
